c0406e0ddbd82d759903817f8e18035d659e0fa6
denoc1 <176614645+denoc1@users.noreply.github.com>
2024-10-24 02:25:00 +0000

    Lab 14 template

---
+++ Lab14.java
@@ -0,0 +1,38 @@
+/*
+    Header Documentation Here
+ */
+public class Lab14{
+
+// name of the method and a brief description.  Include any methods it relies upon.
+// preconditions:  parameters that are being passed and what is expected of them
+// postconditions: what is being returned and what type it is.  If there are any special traits of the return value, state it here.
+
+/*
+public static int countFactors (int value){
+		    //code you will write
+    }  //end countFactors
+*/
+
+// name of the method and a brief description.  Include any methods it relies upon.
+// preconditions:  parameters that are being passed and what is expected of them
+// postconditions: what is being returned and what type it is.  If there are any special traits of the return value, state it here.
+ /*
+ public static Boolean isPrime(int value) {
+	    	//code you will write – must call countFactors
+  	}  //end isPrime method
+*/
+
+
+// name of the method and a brief description.  Include any methods it relies upon.
+// preconditions:  parameters that are being passed and what is expected of them
+// postconditions: what is being returned and what type it is.  If there are any special traits of the return value, state it here.
+ /*
+ public static int countPrimes(int n){
+	    // code you will write – must call isPrime
+	  }  //end countPrimes method
+*/
+public static void main(String[] args){
+
+}//end main driver class
+
+}//end class
